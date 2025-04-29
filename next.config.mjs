b5e322903ef8b9ef05eca52627a87439d559e66b
ShAhilK2 <shahil.kataria@invennico.com>
2025-04-29 17:06:27 +0530

    fix: config fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080],
     imageSizes: [16, 32, 48, 64, 96],
     minimumCacheTTL: 31536000,
     dangerouslyAllowSVG: false,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '360eyecare.ca',
-        port: '',
-        pathname: '/images/**',
+        protocol: "https",
+        hostname: "360eyecare.ca",
+        port: "",
+        pathname: "/images/**",
       },
     ],
   },
   experimental: {
     optimizeCss: true,
-    optimizePackageImports: ['lucide-react'],
-    webVitalsAttribution: ['CLS', 'LCP', 'FCP', 'FID', 'TTFB', 'INP'],
+    optimizePackageImports: ["lucide-react", "react-icons"],
+    webVitalsAttribution: ["CLS", "LCP", "FCP", "FID", "TTFB", "INP"],
     nextScriptWorkers: true,
   },
   poweredByHeader: false,
@@ -26,33 +26,18 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-    return config;
-  },
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ['lucide-react', 'react-icons'],
+    removeConsole: process.env.NODE_ENV === "production",
   },
   async headers() {
     return [
       {
-        source: '/:all*(svg|jpg|png|webp|avif)',
+        source: "/:all*(svg|jpg|png|webp|avif)",
         locale: false,
         headers: [
           {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, must-revalidate',
-          }
+            key: "Cache-Control",
+            value: "public, max-age=31536000, must-revalidate",
+          },
         ],
       },
     ];
