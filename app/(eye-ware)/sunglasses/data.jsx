040d7b2c9f81b73ed2c63f193047afc787ea7e9b
ShAhilK2 <shahil.kataria@invennico.com>
2025-05-06 18:35:24 +0530

    fix: sunglasses image fix

--- app/(eye-ware)/sunglasses/data.jsx
+++ app/(eye-ware)/sunglasses/data.jsx
@@ -1,106 +1,117 @@
 import {
-  TomFordImage,
-  PradaImage,
-  VersaceImage,
-  LaFontImage,
-  OakleyImage,
-  MauiJimImage,
-  PersolImage,
-  BurberryImage,
-  KateSpadeImage,
-  MichaelKors,
-  RayBanImage1,
-  RayBanImage,
-  RudyProjectImage,
+  Brand1,
+  Brand2,
+  Brand3,
+  Brand4,
+  Brand5,
+  Brand6,
+  Brand7,
+  Brand8,
+  Brand9,
+  Brand10,
+  Brand11,
+  Brand12,
+  Brand13,
+  Brand14,
+  Brand15,
+  Brand16,
+  Brand17,
+  Brand18,
+  Brand19,
+  Brand20,
+  Brand21,
+  Brand22,
+  Brand23,
+  Brand24,
 } from "../../../constants/brand";
 
 export const data = [
   {
-    image: LaFontImage,
+    image: Brand1,
     title: "LaFont",
     description:
       "The La Font brand combines traditional craftsmanship with modern French manufacturing. Each season, they unveil at least 30 new frames into their collection, all of which are inspected by over 100 sets of eyes to ensure only top-quality products are introduced. The La Font family, who still maintain full control of their company, believes that a great pair of glasses is an extension of the individual wearing them. Therefore, they strive to release frames that appeal to each individual uniquely.",
     url: "https://www.lafont.com/",
   },
   {
-    image: TomFordImage,
+    image: Brand2,
     title: "Tom Ford",
     description:
       "Tom Ford Sunglasses offer refined design, intelligent glamour, and innovative details. All Tom Ford Sunglasses feature lenses of extraordinary quality and are handmade in Italy. If you are looking for top-quality eyewear that will have people taking notice, Tom Ford is the brand for you.",
     url: "https://www.tomfordfashion.com/en-us/404",
   },
   {
-    image: MauiJimImage,
+    image: Brand3,
     title: "Maui Jim",
     description:
       "Featuring their patented PolarizedPlus2 technology, Maui Jim sunglasses provide vivid colours, crystal clarity through the water, and features that help to reduce squinting and bounce-back glare from behind.",
     url: "https://www.mauijim.com/CA/en_GB",
   },
   {
-    image: OakleyImage,
+    image: Brand4,
     title: "Oakley",
     description:
       "Oakley extended its position as a sports eyewear brand into apparel and accessories, offering men’s and women’s product lines that appeal to sports performance, active, and lifestyle consumers. As of December 31, 2018, the Group operated 361 Oakley “O” stores worldwide (including 81 franchised locations).",
     url: "http://oakley.com/en-ca/category/sunglasses",
   },
   {
-    image: PersolImage,
+    image: Brand5,
     title: "Persol",
     description:
       "They are a line that has been correlated with expressing oneself artistically. They have combined culture, passion, and dedication in creating their eyewear, and they always combine form and function in their design process as well.",
     url: "http://persol.com/canada",
   },
   {
-    image: RayBanImage1,
+    image: Brand6,
     title: "Ray-Ban",
     description:
       "With the classic Wayfarer, Pilot, and Clubmaster frames, RayBan prides itself on being fashion-forward, markets itself as the line everyone wants to be seen in, and is still a powerhouse in the sunglasses world even today.",
     url: "https://www.ray-ban.com/canada/en",
   },
   {
-    image: BurberryImage,
+    image: Brand7,
     title: "Burberry",
     description:
       "TAt 360 Eyecare, we have a variety of Burberry frames and sunglasses marked by their ever-popular and classic check pattern.",
     url: "http://ca.burberry.com/l/womens-sunglasses/",
   },
   {
-    image: KateSpadeImage,
+    image: Brand8,
     title: "Kate Spade",
     description:
       "They offer a wide selection of eyewear in lovely shapes, colours, and styles. They are also a very popular brand for petite faces that find it difficult to get the right fit with glasses.",
     url: "http://katespade.com/shop/accessories/sunglasses-reading-glasses",
   },
   {
-    image: MichaelKors,
+    image: Brand9,
     title: "Michael Kors",
     description:
       "Their frames come in a wide assortment of shapes and often contain embellishments that are signatures of their runway collections. Their frames range from simplistic to decadent, and there is certainly something everyone can enjoy.",
     url: "https://www.michaelkors.com/ca/en/women/accessories/sunglasses/",
   },
   {
-    image: RayBanImage,
+    image: Brand10,
     title: "Ray-Ban Kids",
     description:
       "Discover the latest Ray-Ban sunglasses for kids. With that same great style in the perfect size and designed specifically for kids – you can choose from classic to trendy frames and lenses. Adults aren’t the only ones needing snazzy eyewear, so allow us to introduce the Ray-Ban Junior collection – a dazzling assortment of their most classic styles, all in miniature size.",
     url: "http://ray-ban.com/canada/en/eyeglasses/junior/plp",
   },
   {
-    image: VersaceImage,
+    image: Brand11,
     title: "Versace",
     description:
       "The Versace Group is the epitome of sensual glamour, sexy, and empowering—a true symbol of Italian luxury. At its heart is Versace, a leading design house and a center of creativity flourishing globally.",
     url: "https://www.versace.com/us/en/women/accessories/eyewear/sunglasses/",
   },
   {
-    image: PradaImage,
+    image: Brand12,
     title: "Prada",
     description:
       "Prada represents the best of Italian culture and tradition. It is one of the most innovative, prestigious, and widely recognized brands in the fashion and luxury goods industries. With a keen attention to detail and new trends, Prada’s eyewear collection reflects this approach with unmistakable style, refined elegance, and uncompromising quality.",
     url: "http://prada.com/ca/en/women/sunglasses.html",
   },
   {
-    image: RudyProjectImage,
+    image: Brand13,
     title: "Rudy Project",
     description:
       "Rudy Project is an established brand primarily because of its performance sunglasses, RX solutions, goggles, and helmets, which are of the highest technical level.",
