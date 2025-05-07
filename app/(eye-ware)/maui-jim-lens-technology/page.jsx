fc206623a4a76e98b653b8a95be6faf6d35941e1
prabhanjan sharma <90155386+prab002@users.noreply.github.com>
2025-05-07 17:16:50 +0530

    fix:merge fix the few pages

--- app/(eye-ware)/maui-jim-lens-technology/page.jsx
+++ app/(eye-ware)/maui-jim-lens-technology/page.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import SubHeader from "../../../components/SubHeader";
 import TitleCommon from "../../../components/common/TitleCommon";
-import { Brand6, MauiJimImage } from "../../../constants/brand";
+import { Brand6 } from "../../../constants/brand";
 import {
   MauiJimGolf,
   MjGlassesBeachReflection,
@@ -51,24 +51,21 @@ const page = () => {
     },
   ];
   return (
-    <main className="pt-[80px] md:pt-[110px]">
+    <main className="pt-[110px] bg-[#F6F7F5]">
       <SubHeader text="Maui Jim Lens Technology" />
-      <div className="max-w-[1172px] mx-auto px-4 md:px-6 lg:px-8">
-        <div className="flex flex-col md:flex-wrap md:flex-row justify-between items-center">
-          <div className="w-full md:w-[40%] mb-4 md:mb-0">
+      <div className="max-w-[1172px] w-full px-4 sm:px-6 mx-auto">
+        <div className="flex flex-col md:flex-row flex-wrap justify-between items-center my-4 gap-6">
+          <div className="w-full md:w-[45%]">
             <TitleCommon
               title="Maui Jim: What lenses are best suited for your outdoor Lifestyle ? "
               position="start"
+              css={"text-[30px] leading-[1.2]"}
             />
           </div>
-          <div className="w-full md:w-auto flex justify-center mb-6 md:mb-0">
-            <Image
-              src={Brand6}
-              alt="MauiJimImage"
-              className="max-w-full h-auto"
-            />
+          <div className="w-full md:w-[45%] mx-auto md:mx-0">
+            <Image src={Brand6} alt="MauiJimImage" />
           </div>
-          <p className="text-neutral-500 text-sm md:text-base">
+          <p className="text-neutral-500 text-base">
             All of Maui Jim's sunglasses come polarized as a standard. Based on
             your lifestyle and the sports you may play Maui Jim has specifically
             designed lenses to enhance the colors of the great outdoors. Some of
@@ -79,7 +76,7 @@ const page = () => {
             the polarized tints. This guide is to help you to better consider
             the proper tints and materials based on your outdoor activities.
           </p>
-          <p className="text-neutral-500 text-sm md:text-base my-4">
+          <p className="text-neutral-500 text-base">
             As all Maui Jim lenses are polarized, it is important you factor in
             what polarized tint is best for. For example, if you golf quite
             frequently you may want to consider a polarizedplus 2 lens with a
@@ -87,27 +84,24 @@ const page = () => {
             the course.
           </p>
         </div>
-
-        <div className="mt-8 md:mt-12">
-          <TitleCommon
-            title="The tints Maui Jim has available to order are:"
-            position="start"
-          />
-        </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 mt-6">
+        <TitleCommon
+          title="The tints Maui Jim has available to order are:"
+          position="start"
+          css={"text-[30px] leading-[1.2]"}
+        />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-6 md:gap-8 mt-6 sm:mt-8">
           {data1.map((item, index) => (
             <div key={index} className="w-full">
               <TitleCommon
                 title={item.title}
                 position="start"
-                css="font-[400] text-[24px] md:text-[30px]"
+                css="!font-semibold !text-[30px]"
               />
               {item.description &&
                 item.description.map((desc, idx) => (
                   <p
                     key={idx}
-                    className="text-neutral-500 text-sm md:text-base mt-2"
+                    className="text-neutral-500 text-base sm:text-base mt-2"
                   >
                     {desc}
                   </p>
@@ -115,20 +109,19 @@ const page = () => {
             </div>
           ))}
         </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 mt-6 md:mt-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-6 md:gap-8 mt-6 sm:mt-8">
           {data2.map((item, index) => (
             <div key={index} className="w-full">
               <TitleCommon
                 title={item.title}
                 position="start"
-                css="font-[400] text-[24px] md:text-[30px]"
+                css="!font-semibold !text-[30px]"
               />
               {item.description &&
                 item.description.map((desc, idx) => (
                   <p
                     key={idx}
-                    className="text-neutral-500 text-sm md:text-base mt-2"
+                    className="text-neutral-500 text-base sm:text-base mt-2"
                   >
                     {desc}
                   </p>
@@ -136,20 +129,19 @@ const page = () => {
             </div>
           ))}
         </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 mt-6 md:mt-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-6 md:gap-8 mt-6 sm:mt-8 md:mt-10">
           {data3.map((item, index) => (
             <div key={index} className="w-full">
               <TitleCommon
                 title={item.title}
                 position="start"
-                css="font-[400] text-[24px] md:text-[30px]"
+                css="!font-semibold !text-[30px]"
               />
               {item.description &&
                 item.description.map((desc, idx) => (
                   <p
                     key={idx}
-                    className="text-neutral-500 text-sm md:text-base mt-2"
+                    className="text-neutral-500 text-sm sm:text-base mt-2"
                   >
                     {desc}
                   </p>
@@ -157,73 +149,70 @@ const page = () => {
             </div>
           ))}
         </div>
-
-        <div className="flex flex-col md:flex-row items-center md:items-start gap-6 md:gap-8 mt-6 md:mt-10">
-          <div className="w-full md:max-w-[50%] flex flex-col items-center md:items-start">
-            <div className="mb-4 w-full flex justify-center md:justify-start">
+        <div className="flex flex-col lg:flex-row items-start lg:items-center gap-6 md:gap-8 mt-8">
+          <div className="w-full lg:max-w-[50%] flex flex-col items-start">
+            <div className="w-full">
               <Image
                 src={MjGlassesBeachReflection}
                 alt="MjGlassesBeachReflection"
-                className="max-w-full h-auto"
+                className="w-full h-auto"
               />
             </div>
             <TitleCommon
               title="Maui Pure"
               position="start"
-              css="font-[400] text-[24px] md:text-[30px] text-center md:text-left"
+              css="!font-semibold !text-[30px]"
             />
-            <p className="text-neutral-500 text-sm md:text-base mt-2 text-center md:text-left">
+            <p className="text-neutral-500 text-base sm:text-base mt-2">
               Offers great optics but in addition to tints it is
               shatter-resistant, lightweight, and has a great scratch coat.
             </p>
           </div>
-
-          <div className="w-full md:max-w-[50%] flex flex-col items-center md:items-start">
+          <div className="w-full lg:max-w-[50%] flex flex-col items-start">
             <TitleCommon
               title="Maui Rose"
               position="start"
-              css="font-[400] text-[24px] md:text-[30px] text-center md:text-left"
+              css="!font-semibold !text-[30px]"
             />
-            <p className="text-neutral-500 text-sm md:text-base mt-2 text-center md:text-left">
+            <p className="text-neutral-500 text-base sm:text-base mt-2">
               This tone is said to be the most comforting to the eyes, with its
               high contrast rose colour it makes it easier to further sharpen
               the focus while outdoors.
             </p>
-            <p className="text-neutral-500 text-sm md:text-base mt-2 text-center md:text-left">
+            <p className="text-neutral-500 text-base sm:text-base mt-2">
               When selecting a material it is important that you take into
               account what activities you may be doing outdoors and your
               prescription. When visiting your optician or optometrist they
               often recommend or will prescribe you the most suitable material
               based on your lifestyle.
             </p>
-            <p className="text-neutral-500 text-sm md:text-base mt-2 text-center md:text-left">
+            <p className="text-neutral-500 text-base sm:text-base mt-2">
               Super thin glass when paired with these tints offers the wearer
               the best optical experience. However, it is a bit heavier than
               plastic but still lighter and thinner than regular glass.
             </p>
           </div>
         </div>
-
-        <div className="flex flex-col md:flex-row items-center md:items-start gap-6 md:gap-8 mt-6 md:mt-10">
-          <div className="w-full md:max-w-[46%] gap-6 md:gap-8 flex flex-col items-center md:items-start justify-start">
-            <div className="w-full">
+        <div className="flex flex-col lg:flex-row items-start gap-6 md:gap-8 mt-8">
+          <div className="w-full lg:max-w-[46%] gap-6 md:gap-8 flex flex-col items-start justify-start">
+            <div>
               <TitleCommon
                 title="Maui Evolution"
                 position="start"
-                css="font-[400] text-[24px] md:text-[30px] text-center md:text-left"
+                css="!font-semibold !text-[30px]"
               />
-              <p className="text-neutral-500 text-sm md:text-base mt-2 text-center md:text-left">
+              <p className="text-neutral-500 text-base sm:text-base mt-2">
                 The lightweight polycarbonate lens has great optics and is more
                 impact-resistant compared to other lenses.
               </p>
             </div>
-            <div className="w-full">
+            <div>
               <TitleCommon
                 title="Maui Brilliant"
                 position="start"
-                css="font-[400] text-[24px] md:text-[30px] text-center md:text-left"
+                css="!font-semibold !text-[30px]"
               />
-              <p className="text-neutral-500 text-sm md:text-base mt-2 text-center md:text-left">
+              <p className="text-neutral-500 text-base sm:text-base mt-2">
                 One of Maui Jim's feature lenses as it optimizes optics and
                 weight. The advanced technology in this lens provides the wearer
                 with phenomenal optics combined with 1/3 the conventional
@@ -231,17 +220,15 @@ const page = () => {
               </p>
             </div>
           </div>
-
-          <div className="w-full md:max-w-[50%] flex justify-center md:justify-start">
+          <div className="w-full lg:max-w-[50%] flex flex-col items-start justify-start">
             <Image
               src={MauiJimGolf}
               alt="MauiJimGolf"
-              className="max-w-full h-auto"
+              className="w-full h-auto"
             />
           </div>
         </div>
-
-        <p className="text-neutral-500 text-sm md:text-base my-5 text-center md:text-left">
+        <p className="text-neutral-500 text-base sm:text-base my-5 mb-8">
           Taking this into careful consideration helps you choose the option
           that best suits your lifestyle for optimum vision and comfort when
           choosing your next eyeglasses.
