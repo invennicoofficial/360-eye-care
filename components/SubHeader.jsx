4851fbd6f04a52758ec25ec25190ffa6e3ab00e1
ShAhilK2 <shahil.kataria@invennico.com>
2025-05-09 17:19:48 +0530

    fix: subheader text-wrap in mobile ui

--- components/SubHeader.jsx
+++ components/SubHeader.jsx
@@ -1,7 +1,7 @@
 const SubHeader = ({ text = "Optometrists" }) => {
   return (
     <div className="w-full bg-brand-subheader py-20 flex justify-center items-center">
-      <h1 className="text-white text-3xl md:text-[40px] font-bold text-center font-poppins leading-[46px] ">
+      <h1 className="text-white text-3xl md:text-[40px] font-bold text-center font-poppins leading-[46px] text-wrap ">
         {text}
       </h1>
     </div>
